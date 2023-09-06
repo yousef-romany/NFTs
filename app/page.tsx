694f89f8fb616aa07f4cb2e53d5b2662bb9de923
yousef-romany <yousefromany527@gmail.com>
2023-09-07 00:47:13 +0300

    sad

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Index from "@/components/index/Index";
 import LiveAction from "@/components/index/LiveAction";
 import Popular from "@/components/index/Popular";
 import Ready from "@/components/index/Ready";
-import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,13 +16,6 @@ export default function Home() {
       <Popular />
       <Creators />
       <Ready />
-      <div className="w-full h-fit p-[20px] flex justify-center items-center"> 
-        <h1 className="text-white">
-          Created By : <Link href={"https://yousefromany-dev-93bn.onrender.com/"}>
-            yousef romany.
-          </Link>
-        </h1>
-      </div>
     </>
   );
 }
